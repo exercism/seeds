2a852c5bcc0148abf047c7d5d69e76f8e215b4be
Katrina Owen <katrina.owen@gmail.com>
2014-03-07 06:28:13 -0700

    Refactor user

--- seeds.rb
+++ seeds.rb
@@ -2,6 +2,9 @@ require 'yaml'
 require 'sequel'
 require 'pg'
 
+SOURCE = Sequel.connect('postgres://exercism:@localhost/exercism_development')
+TARGET = Sequel.connect('postgres://exercism:@localhost/exercism_seeds')
+
 libraries = Dir[File.expand_path('../seeds/**/*.rb', __FILE__)]
 libraries.each do |path_name|
   require path_name
--- seeds/user.rb
+++ seeds/user.rb
@@ -1,32 +1,34 @@
-class User
-  attr_reader :at, :username, :attributes, :id
-
-  def initialize(username, attributes={})
-    @at = Timestamp.random
-    @username = username
-    @attributes = default_attributes.update(attributes)
+class User < OpenStruct
+  def self.create(username, attributes={})
+    user = new({username: username}.merge(attributes))
+    id = TARGET[:users].insert(user.to_h)
+    user.id = id
+    user
   end
 
-  def save_to(db)
-    @id = db[:users].insert(attributes)
-    self
+  def initialize(attributes)
+    super(default_attributes.update(attributes))
   end
 
   def submit(n, source_db, destination_db, language=nil)
-    20.times do
+    n.times do
       Iterations.new(self, *Exercise.random(source_db, language)).save_to(destination_db)
     end
   end
 
   private
 
+  def at
+    @at ||= Timestamp.random
+  end
+
   def default_attributes
     {
-      username: username,
       key: Key.user,
       github_id: GitHub.id,
       created_at: at,
-      updated_at: at
+      updated_at: at,
+      mastery: [].to_yaml
     }
   end
 end
