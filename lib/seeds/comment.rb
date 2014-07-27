bf6940d5e1f22413df169e9a660dc608c94f8a50
Katrina Owen <katrina.owen@gmail.com>
2014-07-26 23:06:17 -0700

    Add onboarded tracking

--- lib/seeds/comment.rb
+++ lib/seeds/comment.rb
@@ -13,5 +13,26 @@ class Comment
       LifecycleEvent.track('received_feedback', submission.user_id, at)
       LifecycleEvent.track('commented', user_id, at)
     end
+
+    sql = <<-SQL
+    SELECT commented_at FROM
+    (
+      SELECT MIN(c.created_at) AS commented_at
+      FROM comments c
+      INNER JOIN submissions s
+      ON c.submission_id=s.id
+      WHERE c.user_id != s.user_id
+      AND c.user_id=#{user_id}
+      GROUP BY s.user_id
+    ) t
+    ORDER BY commented_at ASC
+    LIMIT 1 OFFSET 2
+    SQL
+    result = TARGET[sql].to_a.first
+    return if result.nil?
+
+    at = result[:commented_at]
+    TARGET[:users].where(id: user_id).update(onboarded_at: at)
+    LifecycleEvent.track('onboarded', user_id, at)
   end
 end
